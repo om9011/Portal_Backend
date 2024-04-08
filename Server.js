128889f6d454e3676d023393c8b904f9ceca0603
satwikdivate <satwikdivate@gmail.com>
2024-04-08 21:49:08 +0530

    Db connection missing added

--- Server.js
+++ Server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors=require("cors")
+// for db connection
+const db = require("./config/db");
 const port = process.env.PORT || 4000;
 const userRouter=require("./router/userRouter");
 const personalProfileRouter=require("./router/Profile");
 const fileUpload=require("express-fileupload");
 const { cloudinaryConnect } = require("./config/cloundinary");
+
+const {mongoose}=require("./config/db")
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(express.json());
